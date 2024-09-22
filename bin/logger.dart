206625be906aba69aaa8d616a440e4624df9b799
Louis Poirier <contact@louispoirier.io>
2024-09-22 19:23:29 +0200

    chore: add option to log without a newline.

--- bin/logger.dart
+++ bin/logger.dart
@@ -1,23 +1,29 @@
+import 'dart:io';
+
 import 'package:chalkdart/chalkstrings.dart';
 
 class Logger {
-  static log(String message) {
-    _prefix('', message);
+  static log(String message, {bool withoutNewline = false}) {
+    _prefix('', message, withoutNewline);
   }
 
-  static info(String message) {
-    _prefix('ⓘ '.blue, message);
+  static info(String message, {bool withoutNewline = false}) {
+    _prefix('i '.blue, message, withoutNewline);
   }
 
-  static error(String message) {
-    _prefix('✗ '.bold.red, message.bold);
+  static error(String message, {bool withoutNewline = false}) {
+    _prefix('✗ '.bold.red, message.bold, withoutNewline);
   }
 
-  static done(String message) {
-    _prefix('✓ '.green, message);
+  static done(String message, {bool withoutNewline = false}) {
+    _prefix('✓ '.green, message, withoutNewline);
   }
 
-  static _prefix(String prefix, String message) {
-    print('$prefix$message');
+  static _prefix(String prefix, String message, bool withoutNewline) {
+    if (withoutNewline) {
+      stdout.write('$prefix$message');
+    } else {
+      print('$prefix$message');
+    }
   }
 }
