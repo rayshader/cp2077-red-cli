d9241f4f7316f514f21dcbbb2149379bfca66ee6
Louis Poirier <contact@louispoirier.io>
2024-09-25 13:53:05 +0200

    feat: improve time formatting.

--- bin/format.dart
+++ bin/format.dart
@@ -2,11 +2,20 @@ String formatTime(int time) {
   String text;
 
   if (time < 1000) {
-    text = '$time ms';
+    text = '${time}ms';
   } else if (time < 60 * 1000) {
-    text = '${(time / 1000).toStringAsFixed(3)} s';
+    text = '${(time / 1000).truncate()}s';
+  } else if (time < 3600 * 1000) {
+    final minutes = (time / 60 / 1000).truncate();
+    final seconds = (time / 1000 % 60).truncate();
+
+    text = '${minutes}m ${seconds}s';
   } else {
-    text = '${(time / 60 / 1000).toStringAsFixed(0)} m';
+    final hours = (time / 3600 / 1000).truncate();
+    final minutes = (time / 60 / 1000 % 60).truncate();
+    final seconds = (time / 1000 % 60).truncate();
+
+    text = '${hours}h ${minutes}m ${seconds}s';
   }
   return text;
 }
